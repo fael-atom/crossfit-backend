599ca17a1b8413353fd6545c63a2aedc26723a4d
Rafael Augusto Mota <rafaelmota.dev@gmail.com>
2024-04-03 01:17:37 -0300

    feat: add swagger ui structure

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
-import express from 'express';
-import clientRouter from './routes/clientRoutes.js';
-import { corsMiddleware } from './middlewares/cors.js';
-import cors from 'cors'
-
+import express from "express";
+import clientRouter from "./routes/clientRoutes.js";
+import { corsMiddleware } from "./middlewares/cors.js";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json" assert { type: "json" };
 
 export const app = express();
 app.disable("x-powered-by");
 app.use(express.json());
 app.use(cors());
-app.use('/clients', clientRouter);
+app.use("/clients", clientRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
