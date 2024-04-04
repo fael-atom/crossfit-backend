dedf479c5fe218047cc89e77a5dafbb8af095ac3
Rafael Augusto Mota <rafaelmota.dev@gmail.com>
2024-04-04 18:48:53 -0300

    feat: change docs endpoint and tags

--- src/app.js
+++ src/app.js
@@ -10,4 +10,4 @@ app.disable("x-powered-by");
 app.use(express.json());
 app.use(cors());
 app.use("/clients", clientRouter);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
