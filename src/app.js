794c361cda8888206c8acaeef8b046e14af23e42
Rafael Augusto Mota <rafaelmota.dev@gmail.com>
2024-04-07 03:03:55 -0300

    fix: add request like parameter

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ export const app = express();
 app.disable("x-powered-by");
 app.use(express.json());
 app.use(cors());
-app.get("/", (res) => res.redirect("/docs"));
+app.get("/", (req, res) => res.redirect("/docs"));
 app.use("/users", userRouter);
 app.use("/products", productRouter);
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
